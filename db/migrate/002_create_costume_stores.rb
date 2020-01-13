6e0b04096fb8d409dff57e95ebbcf70c8792478d
Wendy Harris <wendyv510@hotmail.com>
2020-01-13 23:56:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name 
       t.string :location 
-      t.integer :costume_inventory 
+      t.integer :costume_inventory
+      t.integer :number_of_employees 
+      t.string :in_business 
       t.integer :opening_time 
       t.integer :closing_time 
     end 
